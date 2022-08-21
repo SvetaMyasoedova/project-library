05246196b1ad123432afb42e0fa1f4481bc07cd3
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-08-21 14:28:23 +0300

    book card adding was fixed

--- book.js
+++ book.js
@@ -2,52 +2,106 @@ let addButton = document.querySelector(".button");
 let popUp = document.querySelector(".addModal");
 let closeX = document.querySelector(".close");
 
+//open popUp
 addButton.addEventListener("click", function (e) {
   popUp.classList.add("active");
 });
 
+//close popUP
 closeX.addEventListener("click", function (e) {
   popUp.classList.remove("active");
 });
 
 let myLibrary = [];
 
-function Book(title, autor, pages, read) {
+//Book Constructor
+function Book(title, author, pages, read) {
   this.title = title;
-  this.autor = autor;
+  this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${title} by ${autor}, ${pages} pages, ${read}`;
-  };
 }
-
-let form = document.getElementById("form");
 
 let cardsOfBooks = document.querySelector(".cardsOfBooks");
+let newBook;
 
-function addBookToLibrary(event) {
-  event.preventDefault();
+//create the Library
+function addBookToLibrary() {
   let title = document.getElementById("title");
-  let autor = document.getElementById("author");
+  let author = document.getElementById("author");
   let pages = document.getElementById("pages");
-  let isRead = document.getElementById("isRead");
-  myLibrary = [title.value, autor.value, pages.value, isRead.checked];
+  let read = document.getElementById("isRead");
 
-  let [addTitle, addAuthor, addPages, addRead] = myLibrary;
+  newBook = new Book(title.value, author.value, pages.value, read.checked);
+  myLibrary.push(newBook);
+  updateLocalStorage(); //saves updated array in local storage
 
-  if (addRead === false) {
-    document.querySelector(".readButton").classList.add("notRead");
-  }
+  //create a book
+  
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("bookCard");
+    let titleElem = document.createElement("div");
+    let authorElem = document.createElement("div");
+    let pageElem = document.createElement("div");
+
+    //title
+    titleElem.textContent = title.value;
+    titleElem.classList.add("title");
+    bookCard.appendChild(titleElem);
+
+    //author
+    authorElem.textContent = author.value;
+    authorElem.classList.add("author");
+    bookCard.appendChild(authorElem);
+
+    //page
+    pageElem.textContent = pages.value;
+    pageElem.classList.add("pages");
+    bookCard.appendChild(pageElem);
+
+    //buttons
+    let readBtn = document.createElement("button");
+    readBtn.classList.add("readBtn");
+    bookCard.appendChild(readBtn);
+
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.classList.add("removeBtn");
+    bookCard.appendChild(removeBtn);
+
+    if (read.checked === false) {
+      readBtn.textContent = "Not Read";
+      readBtn.classList.add("notRead");
+    } else {
+      readBtn.textContent = "Read";
+      readBtn.classList.remove("notRead");
+    }
+    cardsOfBooks.appendChild(bookCard);
+    bookCard.classList.add("active");
+  
+    readBtn.addEventListener("click", function (e) {
+      readBtn.classList.toggle("notRead")
+    });
+
+    removeBtn.addEventListener("click", function (e) {
+      bookCard.remove()
+    });
 }
-document.querySelector(".readButton").addEventListener("click", function (e) {
-  document.querySelector(".readButton").classList.remove("notRead");
-});
 
-form.addEventListener("submit", addBookToLibrary);
+let form = document.querySelector(".addForm");
 
-let btnSubmit = document.querySelector(".btn");
-btnSubmit.addEventListener("click", function (e) {
+//close popUp and add the card
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
   popUp.classList.remove("active");
-  cardsOfBooks.classList.add("active");
+  addBookToLibrary();
+  form.reset();
 });
+
+
+
+
+//localStorage
+function updateLocalStorage() {
+  localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
+}
